b8ed0da4f5379ab014e62da3f05cdfc4cc4a7419
Aditi Chaudhari <aditi102345@gmail.com>
2025-12-30 17:30:30 +0530

    Add files via upload

---
+++ game 2.c
@@ -0,0 +1,47 @@
+//rock paper scissor   "game"
+
+#include<stdio.h>
+int main(){
+	srand(time(NULL));
+	int player,computer = rand() % 3;
+/*	0--> Snake
+	1--> water
+	2--> gun
+*/
+printf("Choose 0 for snake , 1 for water or 2 for gun : ");
+scanf("%d",&player);
+
+printf("Computer chose %d\n",computer);
+
+if(player==0 && computer==0){
+printf("Its a tie\n");	
+}
+else if(player==0 && computer==1){
+	printf("The player wins\n"); //you may use "you win " or "you lose" instead of player and coputer 
+}
+else if(player==0 && computer==2){
+	printf("The computer wins\n");
+}
+else if(player==1 && computer==0){
+	printf("The computer wins\n");
+}
+else if(player==1 && computer==1){
+	printf("Its a tie\n");
+}
+else if(player==1 && computer==2){
+	printf("The player wins\n");
+}
+else if(player==2 && computer==0){
+	printf("The player wins\n");
+}
+else if(player==2 && computer==1){
+	printf("The computer wins\n");
+}
+else if(player==2 && computer==2){
+	printf("Its a tie\n");
+}
+else{
+	printf("Something went wrong");
+}
+return 0;
+}
